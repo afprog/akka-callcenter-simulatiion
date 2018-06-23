2b881d67f31b94794fdf33ddee479af10134fcae
Andres Alzate <aalzate86@gmail.com>
2018-06-22 21:45:19 -0500

    first commit with Operator Actor and project

---
+++ src/main/java/com/almundo/alzate/test/messages/Messages.java
@@ -0,0 +1,25 @@
+package com.almundo.alzate.test.messages;
+
+public class Messages {
+    public static final class CallReceived{
+
+    }
+
+    public static final class CallFinished{
+        private String attendedBy;
+        private long delay;
+
+        public CallFinished(String attendetBy, long delay) {
+            this.attendedBy = attendetBy;
+            this.delay = delay;
+        }
+
+        public String getAttendedBy() {
+            return attendedBy;
+        }
+
+        public long getDelay() {
+            return delay;
+        }
+    }
+}
---
+++ src/test/java/com/almundo/alzate/test/actors/CallReceiverActorTest.java
@@ -0,0 +1,31 @@
+package com.almundo.alzate.test.actors;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.testkit.javadsl.TestKit;
+import com.almundo.alzate.test.messages.Messages;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import scala.concurrent.duration.FiniteDuration;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class CallReceiverActorTest {
+
+    private ActorSystem system = ActorSystem.create("test");
+
+
+    @Test
+    public void testReplyWithEmptyReadingIfNoTemperatureIsKnown() {
+        TestKit probe = new TestKit(system);
+        ActorRef operatorActor = system.actorOf(CallReceiverActor.props("Operator"));
+        operatorActor.tell(new Messages.CallReceived(), probe.getRef());
+        Messages.CallFinished response = probe.expectMsgClass(new FiniteDuration(10,TimeUnit.SECONDS),Messages.CallFinished.class);
+        assertEquals("Operator", response.getAttendedBy());
+
+    }
+
+}
